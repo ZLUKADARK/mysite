ff9919994a75fcf94d7c7f99a9b6df089254a16a
ZLUKADARK <ahmadcakaev@mail.ru>
2021-02-12 10:34:47 +0600

    12.02.2021

--- core/urls.py
+++ core/urls.py
@@ -3,8 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-<<<<<<< HEAD
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
-=======
->>>>>>> 4a8502e26d49c16bc3553844a31aef4c03dd1611
+    path('post/new/$', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
